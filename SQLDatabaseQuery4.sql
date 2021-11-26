44c3ba1ded34f993dd1aa9a8969463ac0cec045a
anas salagar <alisalagarf@gmail.com>
2021-11-26 21:40:15 +0530

    anas UC2 for adding employee payroll data in database

--- SQLDatabaseQuery4.sql
+++ SQLDatabaseQuery4.sql
@@ -1,3 +1,10 @@
 -----UC1 create database-----
 create database payroll_service072;
 use payroll_service072;
+ -----UC2 Ability create a employee payroll table in the payroll service database-----
+ CREATE TABLE employee_payroll (
+	id int IDENTITY(1,1) PRIMARY KEY,
+	name varchar(200),
+	salary float,
+	startDate date
+	);
