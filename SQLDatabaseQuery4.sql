62c455ddd26dd063b72b514ad0485fc6f1ab454f
anas salagar <alisalagarf@gmail.com>
2021-11-26 22:49:10 +0530

    amir UC2 retrieve particular employee payroll data

--- SQLDatabaseQuery4.sql
+++ SQLDatabaseQuery4.sql
@@ -9,6 +9,10 @@ use payroll_service072;
 	startDate date
 	);
 ----UC3 ability to create employee payroll data service database-----
-INSERT INTO employee_payroll (name,salary,startDate) VALUES ('MSD',9999999.99,'2017-07-07'),('AMIR',1111111.11,'2021-01-15');
+INSERT INTO employee_payroll (name,salary,startDate) VALUES ('MSD',9999999.99,'2017-07-07'),('ANAS',1111111.11,'2021-01-15');
 ----UC4  Ability to retrieve all the employee payroll data-----
 select * from employee_payroll;
+
+---UC5 : Ability to retrieve salary data for particular employee----
+select id,name from employee_payroll where name = 'ANAS';
+select * from employee_payroll where startDate between cast('2020-01-02' as date) and getdate();
